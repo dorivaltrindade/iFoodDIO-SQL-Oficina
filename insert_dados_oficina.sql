030c5038e8cb760c52b48d556f793bada74d47c7
dorivaltrindade <dorivalbarrosti@gmail.com>
2023-08-07 21:52:46 -0300

    Versões preliminares
    Estruturação iniciada

---
+++ insert_dados_oficina.sql
@@ -0,0 +1,13 @@
+-- [01] inserindo dados para tabela Funcionario
+
+-- [02] inserindo dados para tabela Servico
+
+-- [03] inserindo dados para tabela Peca
+
+-- [04] inserindo dados para tabela Produto
+
+-- [05] inserindo dados para tabela Veiculo
+
+-- [06] inserindo dados para tabela OrdemServico
+
+-- [07] inserindo dados para tabela Orcamento
