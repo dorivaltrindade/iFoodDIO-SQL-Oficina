beac595c4eb57eab7099677300fc80f0d34c820c
dorivaltrindade <dorivalbarrosti@gmail.com>
2023-08-09 10:35:56 -0300

    Aprimoramento e esboço.
    Aprimoramento da inserção de dados da tabela Orcamento.
    Estruturação do arquivo script das queries para o banco de dados oficina_db.

---
+++ [08] - QUERIES_oficina_db_diversas.sql
@@ -0,0 +1,30 @@
+use oficina_db;
+
+-- [01] Quantos veículos foram conduzidos para atendimento na oficina?
+
+
+-- [02] Quantos orçamentos foram aprovados?
+
+
+-- [03] Qual é o montante de faturamento da oficina?
+
+
+-- [04] Quantos veículos por marca foram conduzidos para atendimento na oficina?
+
+
+-- [05] Relação de atividades por veículo.
+
+
+-- [06] Qual é o orçamento com o maior número de peças?
+
+
+-- [07] Qual é o orçamento com o maior número de serviços?
+
+
+-- [08] Qual é o orçamento com o maior número de produtos?
+
+
+-- [09] Qual funcionário está mais envolvido com a execução das ordem de serviços?
+
+
+-- [10] Qual é média de quilometragem rodada por ano para cada veículo?
--- insert_dados_oficina.sql
+++ insert_dados_oficina.sql
@@ -87,16 +87,16 @@ use oficina_db;
 		select * from Veiculo;
         
 -- [06] inserindo dados para tabela Orcamento
-		insert into Orcamento 	(	idResponsavel_fk, 	idVeiculo_fk, 	Descricao, 										DataInicio)
-					values 		(	11,					1,				'Instalar pneus novos',							'2023-08-10'),
-								(	11,					2,				'Instalar farois novos',						'2023-08-11'),
-								(	11,					3,				'Trocar oleo do motor',							'2023-08-11'),
-								(	11,					4,				'Corrigir falhas do cambio automatico',			'2023-08-12'),
-								(	11,					5,				'Substituir a embreagem',						'2023-08-13'),
-								(	11,					6,				'Substituir a bateria',							'2023-08-14'),
-								(	11,					1,				'Polimento geral da pintura',					'2023-08-15'),
-								(	11,					5,				'Instalacao do sistema de alarme anti-furto',	'2023-08-16'),
-								(	11,					6,				'Substituicao dos amortecedores',				'2023-08-17');
+		insert into Orcamento 	(	idResponsavel_fk, 	idVeiculo_fk, 	Descricao, 										DataInicio,		Valor,	Aprova,	DataTermino,	FormaPagamento)
+					values 		(	11,					1,				'Instalar pneus novos',							'2023-08-10',	1800,	'S',	'2023-08-10',	default),
+								(	11,					2,				'Instalar farois novos',						'2023-08-11',	1600,	'N',	NULL,			NULL),
+								(	11,					3,				'Trocar oleo do motor',							'2023-08-11',	180,	'S',	'2023-08-11',	'PIX'),
+								(	11,					4,				'Corrigir falhas do cambio automatico',			'2023-08-12',	960,	'S',	'2023-08-13',	default),
+								(	11,					5,				'Substituir a embreagem',						'2023-08-13',	675,	'S',	'2023-08-14',	default),
+								(	11,					6,				'Substituir a bateria',							'2023-08-14',	680,	'S',	'2023-08-14',	default),
+								(	11,					1,				'Polimento geral da pintura',					'2023-08-15',	330,	'S',	'2023-08-17',	default),
+								(	11,					5,				'Instalacao do sistema de alarme anti-furto',	'2023-08-16',	485,	'S',	'2023-08-16',	default),
+								(	11,					6,				'Substituicao dos amortecedores',				'2023-08-17',	595,	'N',	NULL,			NULL);
                                 
 		select * from Orcamento;
         
